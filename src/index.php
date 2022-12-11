04d71819d9e68483a1a28bb29877b38bee7ea9e6
PSNapier <46052087+PSNapier@users.noreply.github.com>
2022-12-11 17:54:10 -0600

    Setting up files

---
+++ src/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>Breeding Roller</title>
+		<link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🦄</text></svg>">
+		<link rel="stylesheet" href="style.css">
+		<script src="roller.js"></script>
+	</head>
+
+	<body>
+		<p>Text :></p>
+		<input type="text" id="sire"></input>
+		<input type="text" id="dam"></input>
+	</body>
+
+</html>
