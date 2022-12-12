d8f660762a354c6e8c0f6f83bd3f561a46012849
PSNapier <46052087+PSNapier@users.noreply.github.com>
2022-12-12 09:59:19 -0600

    Started logic, added toolbox

--- src/js/roller.js
+++ src/js/roller.js
@@ -1,3 +1,32 @@
+class Parent {
+	constructor(parent) {
+		this.geno = $(`#${parent}`).val() !== '' ? $(`#${parent}`).val() : 'Ee Aa';
+	}
+}
+
+class Offspring {
+	constructor(offspring, sire, dam) {
+		this.geno = handleBase(sire, dam);
+		console.log(sire.geno, dam.geno);
+	}
+}
+
+function handleBase(sire, dam) {
+	let output = [];
+
+	let dictionary = [
+		['E', 'e'],
+		['At', 'A+', 'A', 'a'],
+	];
+
+	
+
+	return output;
+}
+
 function roll() {
-	console.log('testing');
+	let sire = new Parent('sire');
+	let dam = new Parent('dam');
+	let offspring = new Offspring('offspring', sire, dam);
+	$('#output').html(`Geno: ${offspring.geno}`);
 }
